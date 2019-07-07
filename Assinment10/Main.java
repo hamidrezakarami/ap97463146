bf65fa4f94ce3d54eb2c505dcdb9a41b1a4c83c5
hamidreza <48476890+hamidrezakarami@users.noreply.github.com>
2019-07-07 13:19:51 +0430

    Update Main.java

--- Assinment10/Main.java
+++ Assinment10/Main.java
@@ -15,26 +15,29 @@ import java.util.stream.Stream;
 
 public class Main {
 
-
+    static int number = 0, total = 0;
     static Scanner sc = new Scanner(System.in);
+    static boolean istrue = true;
+    static Set linksset = new HashSet();
+    static ArrayList<String> linklist = new ArrayList<>();
 
     public static void main(String[] args) {
 
         Main main = new Main();
-        ArrayList<String> linklist = new ArrayList<>();
         String link;
         String savepath;
         int numofpage;
 
-        System.out.println("Please Enter a link:");
+        System.out.println("Please Enter a link:(like : https://www.google.ru/)");
         link = sc.next();
-        System.out.println("please Enter path for save:");
+        System.out.println("please Enter path for save: (D:\\game)");
         savepath = sc.next();
-        System.out.println("how mane page you want download ...............");
+        System.out.println("how many page you want download?");
         numofpage = sc.nextInt();
+        System.out.println("please waiting...");
 
 
-        linklist = main.knowAndRemoveRepetitiveLinks(link);
+        main.knowAndRemoveRepetitiveLinks(link);
 
         main.downloadlink(link, savepath);
         System.out.println("Download links:");
@@ -42,20 +45,24 @@ public class Main {
         for (int i = 0; (i < numofpage) && (i < linklist.size() - 1); i++) {
             System.out.println(linklist.get(i));
             main.downloadlink(linklist.get(i), savepath);
+            if (i % 2 == 0) {
+                System.out.println(number + " links download and " + (total-number) + " links left");
+            }
         }
-        System.out.println("complete");
+        if (istrue)
+            System.out.println("complete  ( " + number + " links download )");
+        else
+            System.out.println("!!! ERROR !!!");
 
     }
 
 
-    public ArrayList<String> knowAndRemoveRepetitiveLinks(String link1) {
+    public void knowAndRemoveRepetitiveLinks(String link1) {
         URL url;
-        Set linksset = new HashSet();
-        ArrayList<String> linklist = new ArrayList<>();
+        String h;
+
         try {
             url = new URL(link1);
-
-
             Document doc = Jsoup.connect(String.valueOf(url)).get();             //links
             Elements links = doc.select("a[href]");
             for (Element link : links) {
@@ -68,14 +75,16 @@ public class Main {
             String l = linksset.toString().replace("[", "");
             l = l.replace("]", "");
             StringTokenizer stoken = new StringTokenizer(l, ",");
-            System.out.println(l);
+            total = linksset.size();
             for (int i = 0; i < linksset.size(); i++) {
-                linklist.add(stoken.nextToken());
+                h=stoken.nextToken();
+                linklist.add(h);
+               // knowAndRemoveRepetitiveLinks(h);
             }
         } catch (Exception e) {
             System.out.println(e);
+            istrue = false;
         }
-        return linklist;
     }
 
 
@@ -101,14 +110,17 @@ public class Main {
                 bufferedWriter.write(line);
                 bufferedWriter.newLine();
             }
+            number++;
             bufferedWriter.close();
             writer.close();
         } catch (Exception e) {
             System.out.println(e);
+            istrue = false;
         } finally {
             try {
                 if (is != null) is.close();
             } catch (IOException ioe) {
+                istrue = false;
             }
         }
     }
